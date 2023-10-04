6cea44dd0b457b1753a3c36dfa7b9296b3165aa7
learningtechcoding <aamirsohail7890789@gmail.com>
2023-10-04 15:03:33 +0500

    first_commit

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Init ScrollMagic
+
+//  <!-- for navbar fixed -->
+
+const navbar = document.querySelector(".navbar");
+const originalOffsetTop = navbar.offsetTop;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > originalOffsetTop) {
+    navbar.classList.add("fixed-top");
+    navbar.classList.add("shadowNav");
+  } else {
+    navbar.classList.remove("fixed-top");
+    navbar.classList.remove("shadowNav");
+  }
+});
+
+//   <!-- for removing the zoon-in-left and right on mobile devices -->
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (window.innerWidth <= 768) {
+    document
+      .querySelectorAll('.specification[data-aos="zoom-in-left"]')
+      .forEach((el) => {
+        el.setAttribute("data-aos", "fade-up");
+      });
+    document
+      .querySelectorAll('.specification[data-aos="zoom-in-right"]')
+      .forEach((el) => {
+        el.setAttribute("data-aos", "fade-up");
+      });
+  }
+});
